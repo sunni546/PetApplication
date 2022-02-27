fb9f41aa134956dad15be9ce2bc093f95506dcbf
Inhwa711 <dlsghk0711@inu.ac.kr>
2022-02-28 04:02:11 +0900

    DB(Info/Emotion/Act)

--- PetApplication/app/src/main/java/com/example/pet/Profile_add.java
+++ PetApplication/app/src/main/java/com/example/pet/Profile_add.java
@@ -3,6 +3,9 @@ package com.example.pet;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -44,31 +47,90 @@ public class Profile_add extends AppCompatActivity {
                 // DB 에 User 저장
                 String userUid = user.getUid();
 
+                //이름
+                EditText name_et = findViewById(R.id.name);
+                String name = name_et.getText().toString();
 
-                // TODO : 통증 이상횟수 추가할 곳이 없어서 일단 여기에서
+                //성별
+                RadioGroup gender_rg = findViewById(R.id.gender);
+                int num = gender_rg.getCheckedRadioButtonId();
+                RadioButton gender_rb = findViewById(num);
+                String gender = gender_rb.getText().toString();
+
+                //나이
+                EditText age_et = findViewById(R.id.age);
+                String age = age_et.getText().toString();
+
+                //Cat/Dog
+                RadioGroup cd_rg = findViewById(R.id.c_d);
+                int num1 = cd_rg.getCheckedRadioButtonId();
+                RadioButton cd_rb = findViewById(num1);
+                String c_d = cd_rb.getText().toString();
+
+                //종
+                EditText breed_et = findViewById(R.id.breed);
+                String breed = breed_et.getText().toString();
+
+                //중성화
+                RadioGroup neuter_rg = findViewById(R.id.neuter);
+                int num2 = neuter_rg.getCheckedRadioButtonId();
+                RadioButton neuter_rb = findViewById(num2);
+                String neuter = neuter_rb.getText().toString();
+
+                //질병
+                RadioGroup disease_rg = findViewById(R.id.disease);
+                int num3 = disease_rg.getCheckedRadioButtonId();
+                RadioButton disease_rb = findViewById(num3);
+                String disease = disease_rb.getText().toString();
+
+                DocumentReference newPet = firebaseFirestore.collection("Users").document(userUid)
+                        .collection("Pets").document(name);
+
+                // TODO : 동물 정보 추가
+                Map<String, String> Info = new HashMap<>();
+                Info.put("name", name);
+                Info.put("gender", gender);
+                Info.put("age", age);
+                Info.put("cat/dog", c_d);
+                Info.put("breed", breed);
+                Info.put("neuter", neuter);
+                Info.put("disease", disease);
+                newPet.collection("Info").document().set(Info)
+                        .addOnFailureListener(e -> Log.w("DB_Pet_Info", "error", e));
+
+                // TODO : 감정
+                Map<String, Object> Emotion = new HashMap<>();
+                Emotion.put("행복/즐거움", 1);
+                Emotion.put("평안/안정", 2);
+                Emotion.put("불안/슬픔", 3);
+                Emotion.put("화남/불쾌", 4);
+                Emotion.put("공포", 5);
+                Emotion.put("공격성", 6);
+                newPet.collection("Emotion").document().set(Emotion)
+                        .addOnFailureListener(e -> Log.w("DB_Pet_Emotion", "error", e));
+
+                // TODO : 활동
+                Map<String, Object> Act = new HashMap<>();
+                Act.put("수면 시간", 1);
+                Act.put("식사 시간", 2);
+                Act.put("운동 시간",3);
+                Act.put("활동 기록 시간", new Timestamp(new Date()));
+                newPet.collection("Act").document().set(Act)
+                        .addOnFailureListener(e -> Log.w("DB_Pet_Act", "error", e));
+
+                // TODO : 통증
                 Map<String, Object> Pain = new HashMap<>();
                 Pain.put("numPain", 1);
                 Pain.put("timePain", new Timestamp(new Date()));
+                newPet.collection("Pains").document().set(Pain)
+                        .addOnFailureListener(e -> Log.w("DB_Pet_Pain", "error", e));
+
+                // TODO : 이상횟수
                 Map<String, Object> AB = new HashMap<>();
                 AB.put("numAB", 2);
                 AB.put("timeAB", new Timestamp(new Date()));
-
-                DocumentReference newPet = firebaseFirestore.collection("Users").document(userUid)
-                        .collection("Pets").document();
-                newPet.collection("Pains").document().set(Pain)
-                        .addOnFailureListener(e -> Log.w("DB_Pet_Pain", "error", e));
                 newPet.collection("AbnormalBehaviors").document().set(AB)
                         .addOnFailureListener(e -> Log.w("DB_Pet_AB", "error", e));
-                // TODO : 여기까지
-
-
-                /*
-                // pet 정보 DB 에 추가
-                DB_Pet db_pet = new DB_Pet();
-                firebaseFirestore.collection("Users").document(userUid)
-                        .collection("Pets").document().set(db_pet)
-                        .addOnFailureListener(e -> Log.w("DB_Pet", "error", e));
-                */
 
                 // 팝업 끄기
                 // Home 화면으로
