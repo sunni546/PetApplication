02b7fdf7ac988a8204a91a54623709045a252a84
Sunhee Kim <76764643+sunni3722@users.noreply.github.com>
2022-03-26 00:27:36 +0900

    Bitmap

--- PetApplication/app/src/main/java/com/example/pet/Home_CCTV.java
+++ PetApplication/app/src/main/java/com/example/pet/Home_CCTV.java
@@ -1,21 +1,30 @@
 package com.example.pet;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import org.videolan.libvlc.LibVLC;
 import org.videolan.libvlc.Media;
 import org.videolan.libvlc.MediaPlayer;
 import org.videolan.libvlc.util.VLCVideoLayout;
 
+import wseemann.media.FFmpegMediaMetadataRetriever;
+
 public class Home_CCTV extends AppCompatActivity
 {
-    private static final String url = "rtsp://172.30.1.56:8554/stream.sdp";
+    private static final String url = "rtsp://211.222.65.31:8554/media";
     // private static final String url = "http://172.30.1.52:8080/video";
 
     private LibVLC libVlc;
     private MediaPlayer mediaPlayer;
     private VLCVideoLayout videoLayout;
+
+    private ImageView imageView;
+    private FFmpegMediaMetadataRetriever mediaMetadataRetriever;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,6 +35,8 @@ public class Home_CCTV extends AppCompatActivity
         libVlc = new LibVLC(this);
         mediaPlayer = new MediaPlayer(libVlc);
         videoLayout = findViewById(R.id.videoLayout);
+
+        imageView = findViewById(R.id.imageView);
     }
 
     @Override
@@ -42,6 +53,17 @@ public class Home_CCTV extends AppCompatActivity
         mediaPlayer.setMedia(media);
         media.release();
         mediaPlayer.play();
+
+        mediaMetadataRetriever = new FFmpegMediaMetadataRetriever();
+        mediaMetadataRetriever.setDataSource(url);
+        mediaMetadataRetriever.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_ALBUM);
+        mediaMetadataRetriever.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_ARTIST);
+        Bitmap b = mediaMetadataRetriever.getFrameAtTime(2000000, FFmpegMediaMetadataRetriever.OPTION_CLOSEST); // frame at 2 seconds
+        byte [] artwork = mediaMetadataRetriever.getEmbeddedPicture();
+
+        // imageView.setImageBitmap(b);
+
+        mediaMetadataRetriever.release();
     }
 
     @Override
