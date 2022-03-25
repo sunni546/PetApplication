e62b5781ce091742fd7f3da4853db01004196b58
Inhwa711 <dlsghk0711@inu.ac.kr>
2022-03-26 00:27:29 +0900

    Bitmap

--- PetApplication/app/src/main/java/com/example/pet/Home_CCTV.java
+++ PetApplication/app/src/main/java/com/example/pet/Home_CCTV.java
@@ -1,18 +1,24 @@
 package com.example.pet;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import org.videolan.libvlc.LibVLC;
 import org.videolan.libvlc.Media;
 import org.videolan.libvlc.MediaPlayer;
 import org.videolan.libvlc.util.VLCVideoLayout;
 
+import wseemann.media.FFmpegMediaMetadataRetriever;
+
 public class Home_CCTV extends AppCompatActivity
 {
-    private static final String url = "rtsp://172.30.1.56:8554/stream.sdp";
+    //private static final String url = "rtsp://172.30.1.56:8554/stream.sdp";
     // private static final String url = "http://172.30.1.52:8080/video";
-
+    private static final String url = "rtsp://211.222.65.31:8554/media";
+    private FFmpegMediaMetadataRetriever mmr;
     private LibVLC libVlc;
     private MediaPlayer mediaPlayer;
     private VLCVideoLayout videoLayout;
@@ -42,6 +48,24 @@ public class Home_CCTV extends AppCompatActivity
         mediaPlayer.setMedia(media);
         media.release();
         mediaPlayer.play();
+
+        /*
+        while (<condition to break loop>) {
+        FFmpegMediaMetadataRetriever mmr = new FFmpegMediaMetadataRetriever();
+        mmr.setDataSource(<stream URL>);
+        Bitmap b = mmr.getFrameAtTime(); // current frame
+        mmr.release();
+        //Pause for 5 seconds
+        Thread.sleep(5000);
+        */
+
+        mmr = new FFmpegMediaMetadataRetriever();
+        mmr.setDataSource(url);
+        mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_ARTIST);
+        Bitmap b = mmr.getFrameAtTime(2000000, FFmpegMediaMetadataRetriever.OPTION_CLOSEST); // frame at 2 seconds
+        byte [] artwork = mmr.getEmbeddedPicture();
+        mmr.release();
+
     }
 
     @Override
@@ -60,6 +84,7 @@ public class Home_CCTV extends AppCompatActivity
 
         mediaPlayer.release();
         libVlc.release();
+
     }
 }
 
